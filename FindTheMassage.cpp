88e2d66790d9ae580719c854a33bbb3e8ab96f32
ad7amstein <adham32003200@gmail.com>
2022-12-21 23:47:17 +0200

    Add the load function "to load the dictionary"

--- FindTheMassage.cpp
+++ FindTheMassage.cpp
@@ -1,6 +1,29 @@
+// Headers
 #include <iostream>
+#include <set>
+#include <vector>
+#include <unordered_set>
+#include <fstream>
+#include <string>
 using namespace std;
+// To store the dictionary in
+set<string>DICTIONARY;
+
+// Load the dictionary
+void LoadDic(set<string>& dic) {
+	// open the dictionary
+	ifstream file;
+	file.open("dictionary.txt", ios::in);
+	// Load word by word
+	string w;
+	while (getline(file, w)) {
+		dic.insert(w);
+	}
+	// Close the dictionary
+	file.close();
+}
 
 int main() {
-
+	// Load the dictionary
+	LoadDic(DICTIONARY);
 }
